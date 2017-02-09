1749e3e8ecfa1420af115c7f2cda4e3af525c632
Michael Voetter <dev-mike@famv.net>
2017-02-08 18:25:02 -0600

    Start adding sorcery

---
+++ src/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+  
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :email, uniqueness: true
+  validates :email, uniqueness: true, email_format: { message: 'has invalid format' } 
+end
