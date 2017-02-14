482737d70c6ea65aa05c00190f5db27015fffbd4
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-13 22:51:45 -0600

    add comment

--- src/test/controllers/dashboard_controller_test.rb
+++ src/test/controllers/dashboard_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get dashboard_url
-    assert_response :found
+    assert_response :found  #responds with 302 redirect to user's profile
   end
 
 end
