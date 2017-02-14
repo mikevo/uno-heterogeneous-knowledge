266d49a2bbac88e27ad503bc4fd318a31676f48d
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-13 23:40:34 -0600

    mark test as false positive

--- src/test/controllers/dashboard_controller_test.rb
+++ src/test/controllers/dashboard_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get dashboard_url
-    assert_response :found  #responds with 302 redirect to user's profile
+    assert_response :found  #false positive.. should return 200
   end
 
 end
