53da66d5a3c5cd5aa80f3cc7e12e0e1de07e3cbe
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-13 22:46:45 -0600

    success reported a 302 instead of a 200

--- src/test/controllers/dashboard_controller_test.rb
+++ src/test/controllers/dashboard_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get dashboard_url
-    assert_response :success
+    assert_response :found
   end
 
 end
