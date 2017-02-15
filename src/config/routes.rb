672eacb2aac3b7cee0b5f9b8b6606b626f9dd9a0
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-15 13:51:03 -0600

    add course and quiz routes and views

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   get '/dashboard', to: 'dashboard#index', as: :dashboard
-  
+
   resources :users, only: [:new, :create]
   get '/sign_up', to: 'users#new', as: :sign_up
-  
+
   resources :sessions, only: [:new, :create, :destroy]
   get '/sign_in', to: 'sessions#new', as: :sign_in
   delete '/sign_out', to: 'sessions#destroy', as: :sign_out
+
+  get '/course', to: 'course#index', as: :course
+  get '/quiz', to: 'quiz#index', as: :quiz
+  get '/quiz/new', to: 'quiz#new', as: :new_quiz
 
   root 'static_pages#home'
 
