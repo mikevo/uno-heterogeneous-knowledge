4aed8ccd74ba86eba54f1a6e3ddb95826985364c
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-22 00:59:57 -0600

    remove role from seed data

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'Foo', email: 'foo@bar.com', password: 'password', role: 'teacher')
-User.create(username: 'Student', email: 'student@hk.com', password: 'password', role: 'student')
+User.create(username: 'Foo', email: 'foo@bar.com', password: 'password')
+User.create(username: 'Student', email: 'student@hk.com', password: 'password')
