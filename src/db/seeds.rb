3a721f98fb232fb33df5a5108de234808945bda7
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-23 12:24:46 -0600

    only seed one user

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'Foo', email: 'foo@bar.com', password: 'password')
 User.create(username: 'Student', email: 'student@hk.com', password: 'password')
