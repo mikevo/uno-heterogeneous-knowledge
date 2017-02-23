74ac60f34883e2c08906729f549db76c98295ac2
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-23 11:51:47 -0600

    rename controller test

--- src/test/controllers/quizzes_controller_test.rb
+++ src/test/controllers/quizzes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class QuizControllerTest < ActionDispatch::IntegrationTest
+class QuizzesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
