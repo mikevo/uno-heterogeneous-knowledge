e1d8a89c8e63ee2e55ce6c1c8b8f6977b6a3182f
Michael Voetter <Michael.Voetter@student.uibk.ac.at>
2017-02-23 20:19:58 -0600

    Add cancancan authorization (#52)
    * add cancancan authorization
    
    * Enforcing authorization by default

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   validates :password, confirmation: true
   validates :email, uniqueness: true
   validates :email, uniqueness: true, email_format: { message: 'has invalid format' } 
+  
+  enum role: {
+    student: 0,
+    teacher: 1,
+    admin: 2
+  }
+  
+  def role?(role_name)
+    role == role_name
+  end
 end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208232446) do
+ActiveRecord::Schema.define(version: 20170220172246) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
+    t.string   "email",                        null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.string   "username"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.integer  "role",             default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
