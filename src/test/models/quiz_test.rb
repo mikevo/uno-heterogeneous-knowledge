b5cb8f521b36b99a8a2cac8353de88bafd795a33
Daniel DeKerlegand <dldekerl@gmail.com>
2017-03-01 15:18:12 -0600

    add basic quiz model test

--- src/test/models/quiz_test.rb
+++ src/test/models/quiz_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class QuizTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save quiz without title" do
+    quiz = Quiz.new
+    assert_not quiz.save
+  end
 end
