cb3819721ea275d494d48f7e64500790d20536e4
Michael Voetter <dev-mike@famv.net>
2017-03-13 13:41:29 -0500

    Add default admin user

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# TODO: change email address
+User.create(username: 'admin', email: 'admin@example.com', password: 'admin', password_confirmation: 'admin', role: 'admin')
