6a54cc01c361106667c93fd7bea520751778d5b5
Michael Voetter <dev-mike@famv.net>
2017-03-15 15:53:29 -0500

    Modify landing page to redirect to sign_in or dashboard

--- src/test/controllers/static_pages_controller_test.rb
+++ src/test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get :/
-    assert_response :success
-    assert_template layout: 'layouts/application'
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get redirect to login if not logged in" do
+    get root_path
+    assert_redirected_to sign_in_url
+  end
+
+  test "should get redirect to dashboard if logged in" do
+    sign_in_user(@user)
+
+    get root_path
+    assert_redirected_to dashboard_url
   end
 
 end
