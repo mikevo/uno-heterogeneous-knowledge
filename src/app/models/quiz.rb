2fae2ef885b10e73fe8220b86e51248bfb5c6d28
Daniel DeKerlegand <dldekerl@gmail.com>
2017-03-22 03:09:29 -0500

    new association for quiz model

--- src/app/models/quiz.rb
+++ src/app/models/quiz.rb
@@ -1,3 +1,5 @@
 class Quiz < ApplicationRecord
   validates :title, presence: true
+  has_many :questions, :dependent => :destroy
+  accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
