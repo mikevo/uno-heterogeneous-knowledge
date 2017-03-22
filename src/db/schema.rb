64e2c5da3940261d6d7fa923e04428e758072172
Daniel DeKerlegand <dldekerl@gmail.com>
2017-03-22 03:07:48 -0500

    new schema

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220172246) do
+ActiveRecord::Schema.define(version: 20170322061905) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "quiz_id"
+    t.index ["quiz_id"], name: "index_questions_on_quiz_id"
+  end
 
   create_table "quizzes", force: :cascade do |t|
     t.text     "title"
