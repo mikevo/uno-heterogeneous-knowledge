5d828f020ac6a99766a0ed37951940ba2bb4f637
Michael Voetter <dev-mike@famv.net>
2017-04-04 18:23:57 -0500

    Fix tests

--- src/test/controllers/courses_controller_test.rb
+++ src/test/controllers/courses_controller_test.rb
@@ -3,19 +3,23 @@ require 'test_helper'
 class CoursesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @course = courses(:one)
+    @admin = users(:admin)
   end
 
   test "should get index" do
+    sign_in_user(@admin)
     get courses_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in_user(@admin)
     get new_course_url
     assert_response :success
   end
 
   test "should create course" do
+    sign_in_user(@admin)
     assert_difference('Course.count') do
       post courses_url, params: { course: { name: @course.name } }
     end
@@ -24,21 +28,25 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show course" do
+    sign_in_user(@admin)
     get course_url(@course)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in_user(@admin)
     get edit_course_url(@course)
     assert_response :success
   end
 
   test "should update course" do
+    sign_in_user(@admin)
     patch course_url(@course), params: { course: { name: @course.name } }
     assert_redirected_to course_url(@course)
   end
 
   test "should destroy course" do
+    sign_in_user(@admin)
     assert_difference('Course.count', -1) do
       delete course_url(@course)
     end
