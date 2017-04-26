40e36244dae9b94345ef77d87418b968019e568f
Michael Voetter <dev-mike@famv.net>
2017-04-26 13:38:32 -0500

    Revert changes in db/migrate/20170322061700_create_answers.rb

--- src/db/migrate/20170322061700_create_answers.rb
+++ src/db/migrate/20170322061700_create_answers.rb
@@ -1,11 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
-      t.string :option
-      t.string :is_correct
+      t.string :content
 
       t.timestamps
     end
-    add_reference :answers, :questions, index: true
   end
 end
