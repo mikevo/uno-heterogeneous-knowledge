66c1f479c670ce4694adddb4ec4435ce4b1c8b58
Michael Voetter <dev-mike@famv.net>
2017-04-26 14:31:44 -0500

    Undo changes in db/seeds.rb

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -6,8 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # TODO: change email address
-User.create(username: 'admin', email: 'admin@example.com', password: 'admin', password_confirmation: 'admin',role: 'admin')
-# teacher = User.create(username: 'teacher', email: 'teacher@example.com', password: 'teacher', password_confirmation: 'teacher', role: 'teacher')
-#
-# course = Course.create! name: "Computer Course", teacher_id: teacher.id
-# Quiz.create! title: "Computer Science ", course_id: course.id
+User.create(username: 'admin', email: 'admin@example.com', password: 'admin', password_confirmation: 'admin', role: 'admin')
