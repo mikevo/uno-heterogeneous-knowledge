83287079e3b3f74296daae63490f44501ba0f7a4
Michael Voetter <dev-mike@famv.net>
2017-04-26 14:24:47 -0500

    Revert changes in app/models/question.rb

--- src/app/models/question.rb
+++ src/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-	has_many :answers
-	belongs_to :quizzes
+	belongs_to :quiz
+	has_many :answers, :dependent => :destroy
 	accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
